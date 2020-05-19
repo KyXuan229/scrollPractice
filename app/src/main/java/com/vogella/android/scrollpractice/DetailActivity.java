676ea4daee8055369a60f39c6666d10352deec0e
KyXuan229 <weeshaun229@gmail.com>
2020-05-19 16:39:17 +0800

    scroll practice test

---
+++ app/src/main/java/com/vogella/android/scrollpractice/DetailActivity.java
@@ -0,0 +1,21 @@
+package com.vogella.android.scrollpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        if (getIntent().hasExtra("com.example.SOMETHING")) {
+            TextView tv = (TextView) findViewById(R.id.secondText);
+            String text = getIntent().getExtras().getString("com.example.SOMETHING");
+            tv.setText(text);
+        }
+    }
+}
---
+++ app/src/main/java/com/vogella/android/scrollpractice/ListAdapter.java
@@ -0,0 +1,46 @@
+package com.vogella.android.scrollpractice;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class ListAdapter extends ArrayAdapter<String> {
+
+    Context context;
+    String[] shopnames;
+    String[] descriptions;
+    int Imgs[];
+    LayoutInflater mInflater;
+
+    public ListAdapter(Context c, String[] i, String[] d, int imgs[]) {
+        super(c, R.layout.list_item, R.id.tvTitle, i);
+        this.context = c;
+        this.shopnames = i;
+        this.descriptions = d;
+        this.Imgs = imgs;
+        mInflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        View v = mInflater.inflate(R.layout.list_item, parent, false);
+        TextView tvTitle = v.findViewById(R.id.tvTitle);
+        TextView tvDesc = v.findViewById(R.id.tvDesc);
+        ImageView ivPhoto = v.findViewById(R.id.ivPhoto);
+
+        String title = shopnames[position];
+        String desc = descriptions[position];
+
+        ivPhoto.setImageResource(Imgs[position]);
+        tvTitle.setText(title);
+        tvDesc.setText(desc);
+
+        return v;
+    }
+
+}
